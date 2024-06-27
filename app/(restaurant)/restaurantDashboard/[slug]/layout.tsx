fe10db3920308e7a5180c7d16fc5d84c3bba8efc
Edson-Atkinson <edson.atk@gmail.com>
2024-06-27 18:19:35 -0300

    feat: add route control for users in restaurant layout

--- app/(restaurant)/restaurantDashboard/[slug]/layout.tsx
+++ app/(restaurant)/restaurantDashboard/[slug]/layout.tsx
@@ -4,6 +4,18 @@ import Menu from "../../_components/menu";
 import { Poppins } from "next/font/google";
 import "@/app/globals.css";
 import { Toaster } from "sonner";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/_lib/auth";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/app/_components/ui/alert-dialog";
+import Link from "next/link";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -20,6 +32,34 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+  if (session?.user.role == "USER") {
+    return (
+      <html lang="en">
+        <body className={poppins.className}>
+          <AuthProvider>
+            <div className="h-full w-full bg-slate-600">
+              <AlertDialog open>
+                <AlertDialogContent className="w-[90%] rounded-lg">
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>Acesso negado!</AlertDialogTitle>
+                    <AlertDialogDescription>
+                      Você não tem permição para acessar essa área do sistema!
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogAction>
+                      <Link href={"/"}>Ok</Link>
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+            </div>
+          </AuthProvider>
+        </body>
+      </html>
+    );
+  }
   return (
     <html lang="en">
       <body className={poppins.className}>
