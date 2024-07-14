5a4b4be534561371d0d1aae25e6b231d58c8cae0
Edson-Atkinson <edson.atk@gmail.com>
2024-07-13 22:11:53 -0300

    feat: add mask for cep, currency, cpf in input component

--- app/_components/ui/input.tsx
+++ app/_components/ui/input.tsx
@@ -1,12 +1,25 @@
 import * as React from "react";
 
 import { cn } from "@/app/_lib/utils";
+import inputMask, { MaskTypes } from "@/app/_helpers/input-mask";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  maskType?: MaskTypes;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, maskType, onChange, ...props }, ref) => {
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+      if (maskType) {
+        const mask = inputMask[maskType];
+        event.currentTarget.value = mask(event);
+      }
+
+      if (typeof onChange === "function") {
+        onChange(event);
+      }
+    }
     return (
       <input
         type={type}
@@ -15,6 +28,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           className,
         )}
         ref={ref}
+        onChange={handleChange}
         {...props}
       />
     );
